3649d695028ddd9359545b24d6daffefd55d2520
Kundan <kundankumar594@gmail.com>
2025-06-17 14:54:04 -0400

    normal subscription solution

--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -2,18 +2,34 @@ import { Component, OnInit } from '@angular/core';
 import { EmployeeService, Employee } from './employee.service';
 import { Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-employees',
   templateUrl: './employees.component.html',
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule],
 })
 export class EmployeesComponent implements OnInit {
-  employees$!: Observable<Employee[]>;
+  employees: Employee[] = [];
+  filteredEmployees: Employee[] = [];
+  positions: string[] = [];
+  selectedPosition: string = '';
 
   constructor(private employeeService: EmployeeService) {}
 
   ngOnInit() {
-    this.employees$ = this.employeeService.getEmployees();
+    this.employeeService.getEmployees().subscribe(data => {
+      this.employees = data;
+      this.positions = Array.from(new Set(data.map(emp => emp.position)));
+      this.applyFilter();
+    });
+  }
+
+  applyFilter() {
+    if (this.selectedPosition) {
+      this.filteredEmployees = this.employees.filter(emp => emp.position === this.selectedPosition);
+    } else {
+      this.filteredEmployees = this.employees;
+    }
   }
 }
