5ff11fbf4698c9dc6ab57f8894cbdd468521aebe
Kundan <kundankumar594@gmail.com>
2025-06-25 17:43:50 -0400

    sort pipe  example

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -6,11 +6,12 @@ import { ProductCardComponent } from './product-card.component';
 import { AuthDirective } from '../shared/authDirective';
 import { LogDirective } from '../shared/logDirective';
 import { TemperaturePipe } from '../shared/temperature.pipe';
+import { SortPipe } from '../shared/sort.pipe';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [CommonModule, FormsModule, ProductCardComponent,AuthDirective,LogDirective,TemperaturePipe],
+  imports: [CommonModule, FormsModule, ProductCardComponent,AuthDirective,LogDirective,TemperaturePipe,SortPipe],
   templateUrl: './product-list.component.html',
 })
 export class ProductListComponent {
@@ -18,6 +19,14 @@ export class ProductListComponent {
   editProduct: Product | any = {};
   newProduct: Partial<Product> = {};
   isLoggedIn = true; // Replace with real auth logic
+
+   historicTemperatures = [
+    25, 37, 19, -4, 28, 21, 19, 28, 33, 31, 9, 11, 5, -12, -5,
+  ];
+
+  onReset(index: number) {
+    this.historicTemperatures[index] = 18;
+  }
 
   @ViewChild(ProductCardComponent) productCard!: ProductCardComponent;
 
