78ad87d0df405f2c682542505743e7615792a488
Kundan <kundankumar594@gmail.com>
2025-07-16 10:44:35 -0400

    04-deferrable-views-starting-project --lazyload-

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { RouterOutlet } from '@angular/router';
 
 import { HeaderComponent } from './header/header.component';
 import { UsersComponent } from './users/users.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HeaderComponent, UsersComponent, RouterOutlet],
+  imports: [HeaderComponent, WelcomeComponent ,UsersComponent, RouterOutlet],
 })
 export class AppComponent {
 title = 'employee-list';
