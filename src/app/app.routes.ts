58577c67ad28298ca50a27bdc79e40e1c668e658
Kundan <kundankumar594@gmail.com>
2025-07-16 10:31:48 -0400

    lazylaod -- 03-lazy-loading-service -- need to work more

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,18 +3,21 @@ import { EmployeesComponent } from './employees/employees.component';
 import { ProductListComponent } from './products/product-list.component';
 import { PostListComponent } from './posts/post-list.component';
 import { CommentListComponent } from './comments/comment-list.component';
-import { TasksComponent } from './tasks/tasks.component';
+//import { TasksComponent } from './tasks/tasks.component';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
-import { resolveTitle, resolveUserName, UserTasksComponent } from './users/user-tasks/user-tasks.component';
-import { NewTaskComponent } from './tasks/new-task/new-task.component';
+import {
+  UserTasksComponent,
+  resolveTitle,
+  resolveUserName,
+} from './users/user-tasks/user-tasks.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { routes as userRoutes } from './users/users.routes';
+//import { routes as userRoutes } from './users/users.routes';
 import { inject } from '@angular/core';
 
 const dummyCanMatch: CanMatchFn = (route, segments) => {
   const router = inject(Router);
   const shouldGetAccess = Math.random();
-  if (shouldGetAccess < 0.5) {
+  if (shouldGetAccess < 1) {
     return true;
   }
   return new RedirectCommand(router.parseUrl('/unauthorized'));
@@ -27,7 +30,7 @@ export const routes: Routes = [
   { path: 'posts', component: PostListComponent },
   { path: 'comments', component: CommentListComponent },
   
- {
+  {
     path: '', // <your-domain>/
     component: NoTaskComponent,
     // redirectTo: '/users/u1',
