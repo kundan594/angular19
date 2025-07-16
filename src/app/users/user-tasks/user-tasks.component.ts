b14d57355228f223556a04c6bd740a33b1dc48e7
Kundan <kundankumar594@gmail.com>
2025-07-15 21:25:13 -0400

    13-access-route-data-without-inputs -- message has been directly access to input parameter in ciomp

--- src/app/users/user-tasks/user-tasks.component.ts
+++ src/app/users/user-tasks/user-tasks.component.ts
@@ -1,5 +1,6 @@
-import { Component, inject, input } from '@angular/core';
+import { Component, OnInit, inject, input } from '@angular/core';
 import {
+  ActivatedRoute,
   ActivatedRouteSnapshot,
   ResolveFn,
   RouterLink,
@@ -19,6 +20,15 @@ import { UsersService } from '../users.service';
 export class UserTasksComponent {
   userName = input.required<string>();
   message = input.required<string>();
+  // private activatedRoute = inject(ActivatedRoute);
+
+  // ngOnInit(): void {
+  //   this.activatedRoute.data.subscribe({
+  //     next: data => {
+  //       console.log(data);
+  //     }
+  //   })
+  // }
 }
 
 export const resolveUserName: ResolveFn<string> = (
