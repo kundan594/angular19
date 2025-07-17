ac652e90cdaaf2345e3b193c5ec147be44807af1
Kundan <kundankumar594@gmail.com>
2025-07-17 19:19:30 -0400

    zone-pollution

--- src/app/change-detection/counter/counter.component.ts
+++ src/app/change-detection/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, NgZone, OnInit, inject, signal } from '@angular/core';
 
 import { InfoMessageComponent } from '../info-message/info-message.component';
 
@@ -7,14 +7,28 @@ import { InfoMessageComponent } from '../info-message/info-message.component';
   standalone: true,
   templateUrl: './counter.component.html',
   styleUrl: './counter.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [InfoMessageComponent],
 })
-export class CounterComponent {
+export class CounterComponent implements OnInit {
+  private zone = inject(NgZone);
   count = signal(0);
 
   get debugOutput() {
     console.log('[Counter] "debugOutput" binding re-evaluated.');
     return 'Counter Component Debug Output';
+  }
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.count.set(0);
+    }, 4000);
+
+    this.zone.runOutsideAngular(() => {
+      setTimeout(() => {
+        console.log('Timer expired!');
+      }, 5000);
+    });
   }
 
   onDecrement() {
