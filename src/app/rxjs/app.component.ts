eabe57435433d5631f9407466c2892a446d14bf5
Kundan <kundankumar594@gmail.com>
2025-07-17 19:36:32 -0400

    rxjs start

--- src/app/rxjs/app.component.ts
+++ src/app/rxjs/app.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   standalone: true,
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrl: './app.css'
 })
 export class AppComponent7 {
 
