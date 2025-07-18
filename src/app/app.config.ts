ac571720425a7431b232892682cb7a166f960a4d
Kundan <kundankumar594@gmail.com>
2025-07-17 22:24:10 -0400

    12-interceptors

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withComponentInputBinding, withRouterConfig } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HttpEventType, HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 
@@ -8,6 +8,32 @@ import { routes } from './app.routes';
 import { employeesReducer } from './employees/employees.reducer';
 import { EmployeesEffects } from './employees/employees.effects';
 import { API_URL } from './shared/api.token';
+
+
+import { tap } from 'rxjs';
+
+function loggingInterceptor(
+  request: HttpRequest<unknown>,
+  next: HttpHandlerFn
+) {
+  // const req = request.clone({
+  //   headers: request.headers.set('X-DEBUG', 'TESTING')
+  // });
+  console.log('[Outgoing Request]');
+  console.log(request);
+  return next(request).pipe(
+    tap({
+      next: event => {
+        if (event.type === HttpEventType.Response) {
+          console.log('[Incoming Response]');
+          console.log(event.status);
+          console.log(event.body);
+        }
+      }
+    })
+  );
+}
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -22,6 +48,7 @@ export const appConfig: ApplicationConfig = {
     ),
     provideStore({ employees: employeesReducer }),
     provideEffects([EmployeesEffects]),
+    [provideHttpClient(withInterceptors([loggingInterceptor]))],
     { provide: API_URL, useValue: 'https://dummyjson.com' }
   ]
 };
